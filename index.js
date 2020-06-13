db26ca849f0a5e858f92e3654e093ef7598fe6fc
Eva Navarro <emanao@me.com>
2020-06-13 03:18:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.querySelector("main").remove()
 let h1Victory = document.createElement('h1')
 h1Victory.id = "victory"
-let newHeader = document.querySelector("body")
-newHeader.appendChild(h1Victory)
+let newHeader = document.body.appendChild(h1Victory)
