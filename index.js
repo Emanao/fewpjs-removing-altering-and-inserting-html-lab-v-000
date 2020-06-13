5109eb0e6b1959d1ccf027cbfa145f388216d69f
Eva Navarro <emanao@me.com>
2020-06-13 03:21:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ document.querySelector("main").remove()
 let h1Victory = document.createElement('h1')
 h1Victory.id = "victory"
 let newHeader = document.body.appendChild(h1Victory)
+newHeader.textContent = "YOUR-NAME is the champion"
